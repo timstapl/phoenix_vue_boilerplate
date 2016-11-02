281d18303f2a5aacc6bef52e056f2920f919f837
Timothy Stapleton <retrocede@outlook.com>
2016-11-02 08:33:05 -0700

    added catch-all route to hand off to vue-router

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule PhoenixVueBoilerplate.Router do
   scope "/", PhoenixVueBoilerplate do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
