a32951c4b818d34e7f90dd90abd126856fc168a9
Timothy Stapleton <retrocede@outlook.com>
2016-11-04 08:31:00 -0700

    Added basic example of Vue-resource, page hitting api endpoint for data

--- web/static/js/pages/about/about.vue
+++ web/static/js/pages/about/about.vue
@@ -1,9 +1,35 @@
 <template>
-    <div>About Page</div>
+    <div class="about-page">
+        <div> About Page </div>
+        <pre v-show="!isLoading"> {{ about }} </pre>
+    </div>
 </template>
 
 <script>
+    import AboutResource from '../../resources/about';
 
+    export default {
+        created() {
+            this.fetchData();
+        },
+
+        data() {
+            return {
+                isLoading: false,
+                about: {}
+            }
+        },
+
+        methods: {
+            fetchData() {
+                this.isLoading = true;
+                return AboutResource.fetchAbout().then( response => {
+                    this.about = response.data;
+                    this.isLoading = false;
+                });
+            },
+        },
+    }
 </script>
 
 <style lang="scss" scoped>
